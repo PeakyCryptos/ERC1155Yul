545d7ed726095b7298b05b4b9036318164337efb
Suthan Somadeva <106453938+PeakyCryptos@users.noreply.github.com>
2022-11-13 22:50:29 -0500

    linting / uploaded OZ test

--- test/ERC1155.js
+++ test/ERC1155.js
@@ -1,10 +1,7 @@
 const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { ZERO_ADDRESS } = constants;
-
 const { expect } = require('chai');
-
 const { shouldBehaveLikeERC1155 } = require('./ERC1155.behavior');
-const ERC1155Mock = artifacts.require('ERC1155Mock');
 
 contract('ERC1155', function (accounts) {
   const [operator, tokenHolder, tokenBatchHolder, ...otherAccounts] = accounts;
